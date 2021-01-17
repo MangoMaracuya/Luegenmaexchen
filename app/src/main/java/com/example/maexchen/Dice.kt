61cc388a1f85f17fef7261a67e7d67fd58e0afa3
ContiMango <leviflott@hotmail.de>
2021-01-17 22:05:21 +0100

    Added dice images and sound for maexchen.

--- app/src/main/java/com/example/maexchen/Dice.kt
+++ app/src/main/java/com/example/maexchen/Dice.kt
@@ -12,15 +12,11 @@ class Dice {
         die2 = Random.nextInt(1, 6)
     }
 
-    fun calculate_value(): Int {
-        var value = 0
-        if (this.die1 < this.die2) {
-            value = 10 * die2 + die1;
-        } else if (this.die2 < this.die1) {
-            value = 10 * die1 + die2
-        } else {
-            value = 10 * die1 + die2
+    fun calculate_value(): Pair<Int, Int> {
+        return when {
+            (this.die1 < this.die2) -> Pair(this.die2, this.die1)
+            (this.die2 < this.die1) -> Pair(this.die1, this.die2)
+            else -> Pair(this.die1, this.die2)
         }
-        return value;
     }
 }
